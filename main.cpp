46f6318938641cbd1602658ffabd6a1f823f5798
MenuMarino <benjamin.diaz@utec.edu.pe>
2020-11-13 19:29:24 -0500

    Trabajo final

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include "ternaryTree.hpp"
+
+using namespace std;
+
+const string filename = "file.db";
+
+int main() {
+    TernaryTree tt;
+
+    auto start = chrono::high_resolution_clock::now();
+    auto keys = tt.build(filename);
+    auto end = chrono::high_resolution_clock::now();
+    auto executionTime = chrono::duration_cast<chrono::milliseconds>(end - start);
+    auto buildTime = executionTime.count();
+    
+    
+    ifstream file(filename, ios::binary);
+    
+    start = chrono::high_resolution_clock::now();
+    for (auto i : keys) {
+        // No repite palabras en el search
+        cout << "===================" << '\n';
+        cout << "Key: " << i << endl;
+        for (auto direccion_offset : tt.search(i)) {
+            file.seekg(direccion_offset.first, ios::beg);
+            char* buffer = new char[direccion_offset.second - 1];
+            file.read(buffer, direccion_offset.second - 1);
+            cout << " * " << buffer << '\n';
+        }
+        cout << "===================\n\n" << '\n';
+    }
+    end = chrono::high_resolution_clock::now();
+    executionTime = chrono::duration_cast<chrono::milliseconds>(end - start);
+
+    cout << "Keys: " << keys.size() << ".\n";
+    cout << "Build: " << buildTime << " ms.\n";
+    cout << "Search all keys: " << executionTime.count() << " ms.\n";
+    cout << "Reads (promedio): " << reads/keys.size() << ".\n";
+
+    file.close();
+    return 0;
+}
---
+++ ternaryTree.hpp
@@ -0,0 +1,182 @@
+#ifndef TERNARY_TREE_H
+#define TERNARY_TREE_H
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <fstream>
+#include <unordered_map>
+
+unsigned reads = 0;
+
+using namespace std;
+
+struct Node{
+    bool isTerminal = false; // Indica que el anterior es terminal
+    char letra;
+    Node* center_child = nullptr;
+    Node* right_child = nullptr;
+    Node* left_child = nullptr;
+    Node* parent = nullptr;
+    vector<pair<unsigned, unsigned>> filePos;
+
+    Node(){};
+
+    Node(char value) {
+        letra = value;
+    }
+
+    bool hijosValidos() {
+        return center_child || right_child || left_child;
+    }
+};
+
+class TernaryTree{
+    Node* root = nullptr;
+
+    void completar (string value, Node* nodo, unsigned pos, unsigned offset) {
+        size_t i = 0;
+        while (i < value.length()) {
+            Node* hijo = new Node(value[i++]);
+            nodo->center_child = hijo;
+            hijo->parent = nodo;
+            nodo = nodo->center_child;
+        }
+        Node* nil = new Node();
+        nil->isTerminal = true;
+        nodo->center_child = nil;
+        nil->parent = nodo;
+
+        nodo->filePos.push_back({pos, offset});
+    }
+
+public:
+    void insert(string value, unsigned pos = 0, unsigned offset = 0) {
+        if (root) {
+            Node* c_root = root;
+            size_t i = 0;
+            while (i < value.length()) {
+                if (value[i] == c_root->letra) {
+                    if (c_root->center_child->isTerminal) {
+                        if (i < value.length() - 1) {
+                            c_root->center_child->letra = value[++i];
+                            completar(value.substr(i + 1), c_root->center_child, pos, offset);
+                            return;
+                        }
+                    }
+                    c_root = c_root->center_child;
+                    ++i;
+                    if (i == value.length() - 1) {
+                        c_root->filePos.push_back( {pos, offset} );
+                        c_root->center_child->isTerminal = true;
+                    }
+                } else if (value[i] > c_root->letra) {
+                    if (!c_root->right_child) {
+                        Node* tmp = new Node(value[i++]);
+                        c_root->right_child = tmp;
+                        completar(value.substr(i), tmp, pos, offset);
+                        return;
+                    }
+                    c_root = c_root->right_child;
+                } else {
+                    if (!c_root->left_child) {
+                        Node* tmp = new Node(value[i++]);
+                        c_root->left_child = tmp;
+                        completar(value.substr(i), tmp, pos, offset);
+                        return;
+                    }
+                    c_root = c_root->left_child;
+                }
+            }
+        } else {
+            root = new Node(value[0]);
+            Node* c_root = root;
+            size_t i = 1;
+            while (i < value.length()) {
+                Node* hijo = new Node(value[i++]);
+                c_root->center_child = hijo;
+                hijo->parent = c_root;
+                c_root = c_root->center_child;
+            }
+            Node* nil = new Node();
+            nil->isTerminal = true;
+            c_root->center_child = nil;
+            nil->parent = c_root;
+            c_root->filePos.push_back( {pos, offset} );
+        }
+    }
+
+    Node* getRoot() {
+        return root;
+    }
+
+    vector<pair<unsigned, unsigned>> search(string value) {
+        Node* c_root = root;
+        size_t i = 0;
+        while ( i < value.length() && c_root->hijosValidos() ) {
+            ++reads;
+            if (value[i] == c_root->letra) {
+                if (!c_root->center_child)
+                    return {};
+                c_root = c_root->center_child;
+                ++i;
+            } else if (value[i] > c_root->letra) {
+                if (!c_root->right_child)
+                    return {};
+                c_root = c_root->right_child;
+            } else {
+                if (!c_root->left_child)
+                    return {};
+                c_root = c_root->left_child;
+            }
+        }
+        
+        if (c_root->isTerminal) {
+            return c_root->parent->filePos;
+        }
+        else
+            return {};
+    }
+
+    vector<string> build(string file) {
+        unordered_map<string, bool> hash;
+        ifstream myfile (file, ios::binary);
+        vector<string> keys;
+        string key, line;
+        unsigned int pgdir = 0, offset;
+
+        while(getline(myfile, line)) {
+            offset = (unsigned int) myfile.tellg() - pgdir;
+
+            key = getFileNameFromRoute(line);
+            if (!hash[key]) { keys.push_back(key); }
+            hash[key] = true;
+            insert(key, pgdir, offset);
+
+            pgdir = myfile.tellg();
+        }
+
+        myfile.close();
+        return keys;
+    }
+
+private:
+    string getFileNameFromRoute(string s) {
+        string delimiter = "/";
+
+        size_t pos = 0;
+        string token;
+        while ((pos = s.find(delimiter)) != string::npos) {
+            token = s.substr(0, pos);
+            s.erase(0, pos + delimiter.length());
+        }
+
+        if ((pos = s.find_last_of(".pdf")) != string::npos) {
+            token = s.substr(0, pos - 3);
+            return token;
+        }
+        return "error";
+    }
+};
+
+#endif
